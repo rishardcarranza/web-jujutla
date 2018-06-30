53f4a1c38107463ba382dec4e45c4aaff9f5e20f
Richard Carranza <rishardcarranza@gmail.com>
2018-06-29 23:50:20 -0600

    Footer bar added

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 /* Variables globales */
-var $main = $("#main"),
+var $main = $("#main");
 	counter = 1;
 
 $(document).ready(function () {
@@ -15,74 +15,27 @@ $(document).ready(function () {
 menuClick = function () {
 	$("li").on("click",function () {
 		var opt = $(this).attr("id"); // Get "id" attribute of "li" tag
+		
 		$main.hide();
 		// Option to run
-		if (opt == "inicio") {
-			$main.load("pages/inicio.html?nocache="+getRandomValue(), loadSlider);
-		}else{
-			$main.load("pages/"+opt+".html?nocache="+getRandomValue());
+		switch (opt) {
+			case "main":
+				$main.load("pages/inicio.html?nocache="+getRandomValue(), loadSlider);
+				break;
+
+			case "major":
+				$main.load("pages/major.html?nocache="+getRandomValue(), loadSlider);
+				break;
+
+			default:
+
+				break;
 		}
+		
 		$main.fadeIn();
 	});
 }
 
-menuMobClick = function (){
-	$(".menu_bar").on("click",function () {
-		if(counter == 1){
-			$("nav").animate({
-				"left": "0"
-			});
-			counter = 0;
-		} else {
-			counter = 1;
-			$("nav").animate({
-				"left": "-100%"
-			});
-		}
-	});
-};
-
-menuColor = function () {
-	// var arrColors = ["#00d431","#da0000","#dacd00","#da7100","#009de9"]
-	$("#nav1 li").on("mouseover",function () {
-		var opt = $(this).attr("id"); // Get "id" attribute of "li" tag
-		// Change color of menu
-		switch (opt) {
-			case "inicio":
-				$(this).css("background-color","#009de9");
-				break;
-			case "nosotros":
-				$(this).css("background-color","#00d431");
-				break;
-			case "servicios":
-				$(this).css("background-color","#da0000");
-				break;
-			case "noticias":
-				$(this).css("background-color","#e9009d");
-				break;
-			case "galeria":
-				$(this).css("background-color","#dacd00");
-				break;
-			case "contacto":
-				$(this).css("background-color","#da7100");
-				break;
-		}
-	});
-	$("#nav1 li").on("mouseleave",function () {
-		var opt = $(this).attr("id"); // Get "id" attribute of "li" tag
-		// Change default color
-		switch (opt) {
-			case "inicio":
-			case "nosotros":
-			case "servicios":
-			case "noticias":
-			case "galeria":
-			case "contacto":
-				$(this).css("background-color","#7fffd4");
-				break;
-		}
-	});
-}
 
 loadSlider = function () {
 	$('#camera_wrap_1').camera({
