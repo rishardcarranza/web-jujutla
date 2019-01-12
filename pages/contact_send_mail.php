00caae8983959888d28d6c5606a2446328aa0e9e
Richard Carranza <rishardcarranza@gmail.com>
2019-01-12 02:35:42 -0600

    Added send mail function by php mailer library

--- pages/contact_send_mail.php
+++ pages/contact_send_mail.php
@@ -1,3 +1,59 @@
 <?php
-$nombre = $_POST["nombre"];
-?>
+//Import PHPMailer classes into the global namespace
+use PHPMailer\PHPMailer\PHPMailer;
+require '../vendor/phpmailer/Exception.php';
+require '../vendor/phpmailer/PHPMailer.php';
+require '../vendor/phpmailer/SMTP.php';
+
+//Create a new PHPMailer instance
+$mail = new PHPMailer;
+//Tell PHPMailer to use SMTP
+$mail->isSMTP();
+//Enable SMTP debugging
+// 0 = off (for production use)
+// 1 = client messages
+// 2 = client and server messages
+$mail->SMTPDebug = 2;
+//Set the hostname of the mail server
+$mail->Host = 'smtp.gmail.com';
+// use
+// $mail->Host = gethostbyname('smtp.gmail.com');
+// if your network does not support SMTP over IPv6
+//Set the SMTP port number - 587 for authenticated TLS, a.k.a. RFC4409 SMTP submission
+$mail->Port = 465;
+//Set the encryption system to use - ssl (deprecated) or tls
+$mail->SMTPSecure = 'ssl';
+//Whether to use SMTP authentication
+$mail->SMTPAuth = true;
+//Username to use for SMTP authentication - use full email address for gmail
+$mail->Username = "rishardcarranza@gmail.com";
+//Password to use for SMTP authentication
+$mail->Password = "";
+//Set who the message is to be sent from
+$mail->setFrom('info@alcaldiajujutla.com', 'Test Name');
+//Set an alternative reply-to address
+// $mail->addReplyTo('mh18dani@gmail.com', 'Test Name 2');
+$mail->AddCC('mh18dani@gmail.com', 'Daniel Mate');
+//Set who the message is to be sent to
+$mail->addAddress('rishardcarranza@gmail.com', 'Richard Carranza');
+//Set the subject line
+$mail->Subject = 'PHPMailer GMail SMTP test';
+//Read an HTML message body from an external file, convert referenced images to embedded,
+//convert HTML into a basic plain-text alternative body
+// $mail->msgHTML(file_get_contents('contents.html'), __DIR__);
+$mail->msgHTML("<h1>Test Mail from localhost</h1>", __DIR__);
+//Replace the plain text body with one created manually
+$mail->AltBody = 'This is a plain-text message body';
+//Attach an image file
+// $mail->addAttachment('images/phpmailer_mini.png');
+//send the message, check for errors
+if (!$mail->send()) {
+    echo "Mailer Error: " . $mail->ErrorInfo;
+} else {
+    echo "Message sent!";
+    //Section 2: IMAP
+    //Uncomment these to save your message in the 'Sent Mail' folder.
+    #if (save_mail($mail)) {
+    #    echo "Message saved!";
+    #}
+}
